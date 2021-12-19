cbadb25a66c227ea9ea1039b2c411aaae20613bc
Kurtis <gears2525@aol.com>
2021-12-19 14:04:45 -0800

    Added connection to db and fixed model index file

--- models/index.js
+++ models/index.js
@@ -1,27 +1,26 @@
-// import models
-const Product = require('./Product');
 const Category = require('./Category');
-const Tag = require('./Tag');
+const Product = require('./Product');
 const ProductTag = require('./ProductTag');
+const Tag = require('./Tag');
 
-// Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id'
-})
-// Categories have many Products
-Category.hasMany(Product,{
-  foreignKey:'category_id'
-})
-// Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(ProductTag,{
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+
+Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey:'tag_id'
-})
-// Tags belongToMany Products (through ProductTag)
-ProductTag.belongsToMany(Product,{
+  foreignKey: 'product_id'
+});
+
+Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey:'tag_id'
+  foreignKey: 'tag_id'
 })
+
 module.exports = {
   Product,
   Category,
